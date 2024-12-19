8db4b52e43bd4c04d14b072926e72e52b20b1de2
vanitas2126 <148174635+vanitas2126@users.noreply.github.com>
2024-12-19 17:15:23 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let burung = document.getElementById('burung')
+let text = document.getElementById('text')
+
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+    burung.style.left = value * 0.25 + 'px';
+    text.style.marginBottom = value * 1 + 'px';
+  
+});
